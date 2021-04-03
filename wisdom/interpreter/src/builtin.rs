adeace240aa6f35dd8c16945c08ceb6836a69f79
Giles Hutton <hutton.giles@gmail.com>
2021-04-03 14:46:50 +0100

    Added builtin function for dumping the AST in the REPL

--- wisdom/interpreter/src/builtin.rs
+++ wisdom/interpreter/src/builtin.rs
@@ -1,10 +1,12 @@
-use ast::Value;
+use ast::{Value, Stmt};
 
-use crate::error::Error;
+use crate::error::{Error, ErrorKind};
+use tokenizer::{TokenStream, FromTokens};
 
 pub fn exists(name: &String) -> bool {
     match name.as_str() {
         "print" => true,
+        "ast" => true,
         _ => false
     }
 }
@@ -12,6 +14,7 @@ pub fn exists(name: &String) -> bool {
 pub fn run(name: &String, args: Vec<Value>) -> Result<Value, Error> {
     match name.as_str() {
         "print" => print(args),
+        "ast" => ast(args),
         _ => panic!("no such builtin function: {}", name)
     }
 }
@@ -22,4 +25,19 @@ pub fn print(args: Vec<Value>) -> Result<Value, Error> {
     }
     print!("\n");
     Ok(Value::None)
+}
+
+pub fn ast(args: Vec<Value>) -> Result<Value, Error> {
+    use ron::ser::PrettyConfig;
+    let arg = args.iter().next().ok_or(
+        Error::new(ErrorKind::UnexpectedArgs(0, 1))
+    )?;
+    if let Value::String(s) = arg {
+        let tokens = TokenStream::new(&s);
+        let stmt = Stmt::from_tokens(&tokens)?;
+        Ok(Value::String(ron::ser::to_string_pretty(&stmt, PrettyConfig::new()).unwrap_or(
+            String::from("invalid statement"))))
+    } else {
+        Err(Error::new(ErrorKind::InvalidType))
+    }
 }
